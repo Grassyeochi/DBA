ce1106f05fe524e3c9a9da5870a30b801432e089
Grassyeochi <yeochi2000@gmail.com>
2025-09-26 16:26:38 +0900

    ora19 : auto commit at 2025-09-26 16:26:38

--- ora19/coldbackup.sql
+++ ora19/coldbackup.sql
@@ -2,19 +2,21 @@
 
 
 export ORACLE_SID=oracle19
-BACKUP_DIR=/home/oracle/coldbackup
+BACKUP_START_DIR=/u01/app/oracle/oradata/ORACLE19/*
+BACKUP_END_DIR=/home/oracle/coldbackup
+
 sqlplus -s sys/oracle_4U as sysdba << EOF
 
 shutdown immediate
 exit;
 EOF
 
-cp /u01/app/oracle/oradata/ORACLE19/* $BACKUP_DIR/
+cp $BACKUP_START_DIR $BACKUP_END_DIR/
 
 sqlplus -s / as sysdba << EOF
 startup;
 exit;
 EOF
 
-echo "Cold backup completed successfully to $BACKUP_DIR"
+echo "Cold backup completed successfully to $BACKUP_END_DIR"
 
